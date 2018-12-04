5ed7bd499611b1302549668b7c4802bae9ab524f
Nick Coupland <ncouplan@students.kennesaw.edu>
2018-12-04 10:07:25 -0500

    edited items in store

--- db/store_schema.sql
+++ db/store_schema.sql
@@ -13,14 +13,17 @@ CREATE TABLE product (
 );
 
 
-INSERT INTO product (id, name, brand, model, image, lens_diameter, bridge_width, side_length)
-VALUES ("1", "Rubber Eagle sunglasses", "Carrera", "Aviator", "images/image1.jpg", 2.2, 1.5, 2.00 );
+INSERT INTO product (name, brand, model, image, lens_diameter, bridge_width, side_length)
+VALUES ("Rubber Eagle sunglasses", "Carrera", "Aviator", "images/image1.jpg", 2.2, 1.5, 128 );
 
-INSERT INTO product (id, name, brand, model, image, lens_diameter, bridge_width, side_length)
-VALUES ("2", "Carrera Polarized Men", "Carrera", "N/A", "images/image2.jpg", 59, 15, 12.00 );
+INSERT INTO product (name, brand, model, image, lens_diameter, bridge_width, side_length)
+VALUES ("Carrera Polarized Men", "Carrera", "N/A", "images/image2.jpg", 59, 15, 128 );
 
-INSERT INTO product (id, name, brand, model, image, lens_diameter, bridge_width, side_length)
-VALUES ("3", "Oakley Latch Spectrum", "Oakley", "Latch", "images/image3.jpg", 3.2, 11.5, 6.00 );
+INSERT INTO product (name, brand, model, image, lens_diameter, bridge_width, side_length)
+VALUES ("Oakley Latch Spectrum", "Oakley", "Latch", "images/image3.jpg", 3.2, 11.5, 129 );
 
-INSERT INTO product (id, name, brand, model, image, lens_diameter, bridge_width, side_length)
-VALUES ("4", "Ray-Ban Men's Vintage", "Ray-Ban", "N/A", "images/image4.jpg", 12.2, 12, 20 );
+INSERT INTO product (name, brand, model, image, lens_diameter, bridge_width, side_length)
+VALUES ("Ray-Ban Men's Vintage", "Ray-Ban", "N/A", "images/image4.jpg", 12.2, 12, 125 );
+
+INSERT INTO product (name, brand, model, image, lens_diameter, bridge_width, side_length)
+VALUES ("illesteva", "henry", "slat", "images/image4.jpg", 11.9, 12, 135 );
